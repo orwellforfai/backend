2cea0f3b398db9b13bf28ffee2fef2fd4f1540cb
Martin Beltramo <94710842+orwellforfai@users.noreply.github.com>
2023-04-12 11:40:37 -0300

    Version con errores en limit = 5

--- desafio.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const filename = './package.json';
-
-const contenido = JSON.parse(fs.readFileSync(filename, 'utf-8'));
-contenido.author = "Martin Beltramo"
-fs.writeFileSync(filename, JSON.stringify(contenido, null,'\t'));
-
-
---
+++ src/productos.js
@@ -0,0 +1,91 @@
+const fs = require('fs')
+const filename = './productos.json'
+
+class ProductManager {
+    products
+
+    constructor() {
+        this.products = []
+    }
+
+    #generateID = () => {
+        if (this.products.length === 0) return 1
+        return this.products[this.products.length - 1].id + 1
+    }
+
+    addProduct(title, description, price, image, code, stock) {
+        const id = this.#generateID()
+        const product = {id, title, description, price, image, code, stock}
+
+        // Valido que el producto tenga todos los campos
+        if (!title || !description || !price || !image || !code || !stock) {
+            return console.log('Error: Todos los campos son obligatorios')
+        }
+
+        //Valido que el producto ingresado no exista
+        const productExists = this.products.some((product) => product.code === code)
+        if (productExists) {
+            return console.log('Error: El producto ya existe')
+        } else {
+            this.products.push(product)
+            fs.writeFileSync(filename, JSON.stringify(this.products, null, '\t'))
+        }
+    }
+
+    getProducts() {
+
+        // return this.#products
+
+        const contenido = JSON.parse(fs.readFileSync(filename, 'utf-8'))
+        //console.log(" Muestro el contenido del archivo Productos", contenido)
+    }
+
+    getProductsById(id) {
+
+        const contenido = JSON.parse(fs.readFileSync(filename, 'utf-8'))
+
+        console.log("Muestro el contenido del ID elegido", contenido.find(p => p.id === id))
+
+    }
+    updateProduct(id, title, description, price, image, code, stock) {
+        const product = this.products.find((product) => product.id === id)
+        if (!product) {
+            return console.log('Error: El producto no existe')
+        }
+
+        const newProduct = {id, title, description, price, image, code, stock}
+        this.products = this.products.map((product) => product.id === id ? newProduct : product)
+        fs.writeFileSync(filename, JSON.stringify(this.products, null, '\t'))
+    }
+
+    deleteProduct(id) {
+        const product = JSON.parse(fs.readFileSync(filename, 'utf-8'))
+        product.find(p => p.id === id)
+        if (!product) {
+            return console.log('Error: El producto no existe')
+        }
+        const newproduct = product.filter(p => p.id !== id)
+        fs.writeFileSync(filename, JSON.stringify(newproduct, null, '\t'))
+    }
+}
+// exporto la clase ProductManager para poder ser usada en el archivo index.js. Esto resuelve el error TypeError: ProductManager is not a constructor
+module.exports = ProductManager
+
+// const product = new ProductManager()
+// product.addProduct("Producto1", "Descripcion1", "Precio1", "Imagen1", "Codigo1", 1)
+// // product.addProduct("Producto1", "Descripcion1", "Precio1", "Imagen1", "Codigo1", 1)
+// product.addProduct("Producto2", "Descripcion2", "Precio2", "Imagen2", "Codigo2", 2)
+// product.addProduct("Producto3", "Descripcion3", "Precio3", "Imagen3", "Codigo3", 3)
+// product.addProduct("Producto4", "Descripcion4", "Precio4", "Imagen4", "Codigo4", 4)
+// product.addProduct("Producto5", "Descripcion5", "Precio5", "Imagen5", "Codigo5", 5)
+// product.addProduct("Producto6", "Descripcion6", "Precio6", "Imagen6", "Codigo6", 6)
+// product.addProduct("Producto7", "Descripcion7", "Precio7", "Imagen7", "Codigo7", 7)
+// product.addProduct("Producto8", "Descripcion8", "Precio8", "Imagen8", "Codigo8", 8)
+// product.addProduct("Producto9", "Descripcion9", "Precio9", "Imagen9", "Codigo9", 9)
+// product.addProduct("Producto10", "Descripcion10", "Precio10", "Imagen10", "Codigo10", 10)
+
+
+// product.getProducts()
+// product.getProductsById(1)
+// product.updateProduct(1, "Producto1", "Descripcion1", "Precio1", "Imagen1", "Codigo1", 2)
+// product.deleteProduct(1)
